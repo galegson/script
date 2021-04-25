ea5084ac8da892bd7d63ba385c03c0de25da0846
marcel <mgalegson.com>
2021-04-25 17:19:12 -0500

    webserver update

--- webserver.sh
+++ webserver.sh
@@ -3,6 +3,6 @@ systemctl start apache2
 systemctl enable apache2
 ufw allow 80/tcp
 ufw reload
-echo “this is a webserver created from a script. Thanks for stopping by !” > /var/www/html/index.html
+echo “this is a webserver using ubuntu created from a script. Thanks for stopping by !” > /var/www/html/index.html
 
 
